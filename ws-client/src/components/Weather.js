166fc5a088d9ef556f1f5b0db890c8f57fa411d1
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-05-05 16:26:45 +0100

    correct table nesting

--- ws-client/src/components/Weather.js
+++ ws-client/src/components/Weather.js
@@ -8,46 +8,51 @@ function Weather(props) {
     {props.result.cod === 200 ? (
         <div id='ResultBox'>
             <table>
-                <tr>
-                    <td id='Name'>
-                        {props.result.name}
-                    </td>
-                    <td id='LonLat'>
-                        [ {props.result.coord.lon}, {props.result.coord.lat} ]
-                    </td>
-                </tr>
-                <tr>
-                    <td id='Temp'>
-                        {props.result.main.temp} °C
-                    </td>
-                    <td id='TempDesc'>
-                        <img src={`http://openweathermap.org/img/w/${props.result.weather[0].icon}.png`} alt='weather icon'></img>
-                        <p id='DescText'>{props.result.weather[0].description}</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td id='InfoBox'>
-                        <tr>
-                            <td>
-                                Humidity:
-                            </td>
-                            <td>
-                                {props.result.main.humidity} %
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Pressure:
-                            </td>
-                            <td>
-                                {props.result.main.pressure} hpa
-                            </td>
-                        </tr>
-                    </td>
-                    <td>
-                        
-                    </td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td id='Name'>
+                            {props.result.name}
+                        </td>
+                        <td id='LonLat'>
+                            [ {props.result.coord.lon}, {props.result.coord.lat} ]
+                        </td>
+                    </tr>
+                    <tr>
+                        <td id='Temp'>
+                            {props.result.main.temp} °C
+                        </td>
+                        <td id='TempDesc'>
+                            <img src={`http://openweathermap.org/img/w/${props.result.weather[0].icon}.png`} alt='weather icon'></img>
+                            <p id='DescText'>{props.result.weather[0].description}</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td id='InfoBox'>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            Humidity:
+                                        </td>
+                                        <td>
+                                            {props.result.main.humidity} %
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Pressure:
+                                        </td>
+                                        <td>
+                                            {props.result.main.pressure} hpa
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
             
